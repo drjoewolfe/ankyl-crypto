5d4df2d7fdf17761e8a244b329381988c88b886d
drjoewolfe <drjoewolfe@gmail.com>
2019-11-09 16:35:26 +0530

    Updated the dependency to ankyl-swing.

--- src/main/java/com/jwolfe/ankyl/client/CryptoTryoutMain.java
+++ src/main/java/com/jwolfe/ankyl/client/CryptoTryoutMain.java
@@ -12,7 +12,6 @@ import javax.swing.event.ChangeListener;
 import java.awt.event.*;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
