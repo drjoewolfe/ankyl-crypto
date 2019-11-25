f98eb2f18f1d7c4af3d319cf9185f32b7acd0af7
drjoewolfe <drjoewolfe@gmail.com>
2019-11-25 15:16:00 +0530

    Checkstyle fixes++

--- src/main/java/com/jwolfe/ankyl/crypto/CipherAlgorithms.java
+++ src/main/java/com/jwolfe/ankyl/crypto/CipherAlgorithms.java
@@ -1,19 +1,23 @@
 package com.jwolfe.ankyl.crypto;
 
 public final class CipherAlgorithms {
-    public final static String AES = "AES";
-    public final static String AES_WRAP = "AESWrap";
-    public final static String ARCFOUR = "ARCFOUR";
-    public final static String BLOWFISH = "Blowfish";
-    public final static String CCM = "CCM";
-    public final static String DES = "DES";
-    public final static String DES_EDE = "DESede";
-    public final static String DES_EDE_WRAP = "DESedeWrap";
-    public final static String ECIES = "ECIES";
-    public final static String GCM = "GCM";
-    public final static String PBE = "PBE";
-    public final static String RC2 = "RC2";
-    public final static String RC4 = "RC4";
-    public final static String RC5 = "RC5";
-    public final static String RSA = "RSA";
+    private CipherAlgorithms() {
+
+    }
+
+    public static final String AES = "AES";
+    public static final String AES_WRAP = "AESWrap";
+    public static final String ARCFOUR = "ARCFOUR";
+    public static final String BLOWFISH = "Blowfish";
+    public static final String CCM = "CCM";
+    public static final String DES = "DES";
+    public static final String DES_EDE = "DESede";
+    public static final String DES_EDE_WRAP = "DESedeWrap";
+    public static final String ECIES = "ECIES";
+    public static final String GCM = "GCM";
+    public static final String PBE = "PBE";
+    public static final String RC2 = "RC2";
+    public static final String RC4 = "RC4";
+    public static final String RC5 = "RC5";
+    public static final String RSA = "RSA";
 }
