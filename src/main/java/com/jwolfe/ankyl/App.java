ec4da8de320ca2cb120b169b3123f0480f450065
drjoewolfe <drjoewolfe@gmail.com>
2019-12-02 20:39:00 +0530

    Checkstyle fixes++

--- src/main/java/com/jwolfe/ankyl/App.java
+++ src/main/java/com/jwolfe/ankyl/App.java
@@ -2,10 +2,11 @@ package com.jwolfe.ankyl;
 
 import com.jwolfe.ankyl.client.CryptoTryoutMain;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         JFrame frame = new JFrame("Ankyl: Crypto Main");
         frame.setContentPane(new CryptoTryoutMain().cryptoMainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
